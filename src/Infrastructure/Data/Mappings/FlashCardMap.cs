a6ee3931a205781d16815b16b13f2ffacb2f6544
Ever Orellana <ever.orellana@epandcompany.com>
2022-08-02 22:39:30 -0600

    Adding mapping for category and flashcard

--- src/Infrastructure/Data/Mappings/FlashCardMap.cs
+++ src/Infrastructure/Data/Mappings/FlashCardMap.cs
@@ -8,6 +8,16 @@ public class FlashCardMap : IEntityTypeConfiguration<FlashCard>
 {
     public void Configure(EntityTypeBuilder<FlashCard> builder)
     {
+        builder.HasKey(e => e.FlashCardID);
+        builder.Property(e => e.FrontText).HasColumnType("varchar(50)");
+        builder.Property(e => e.CreatedDate).HasColumnType("datetime").IsRequired(false);
+        builder.Property(e => e.BackText).HasMaxLength(100);
+
+        builder.HasOne(e => e.Category)
+            .WithMany(d => d.FlashCards)
+            .OnDelete(DeleteBehavior.ClientSetNull)
+            .HasForeignKey(e => e.CategoryID)
+            .HasConstraintName("FK_FlashCard_Category");
         
     }
 }

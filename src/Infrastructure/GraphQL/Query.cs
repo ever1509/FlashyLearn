f9593e0d40e4ed9d91fe8b6ea891afacf5977f60
Ever Orellana <ever.orellana@epandcompany.com>
2022-08-20 23:30:09 -0600

    calling queries with graphql for allcategories and runflashcards

--- src/Infrastructure/GraphQL/Query.cs
+++ src/Infrastructure/GraphQL/Query.cs
@@ -1,6 +1,18 @@
+using Application.Categories.Dtos;
+using Application.Categories.Queries.AllCategories;
+using Application.FlashCards.Dtos;
+using Application.FlashCards.Queries.RunFlashCards;
+using Domain.Enums;
+using MediatR;
+
 namespace Infrastructure.GraphQL;
 
 public class Query
 {
-    
+    public async Task<List<CategoryDto>>
+        AllCategories([Service] IMediator mediator, int page = 1, string? userId = null) =>
+        await mediator.Send(new AllCategories() {UserId = userId, Page = page});
+
+    public async Task<List<FlashCardDto>> RunFlashCards([Service] IMediator mediator, string userId,
+        Frequency frequency) => await mediator.Send(new RunFlashCards() {UserId = userId, Frequency = frequency});
 }

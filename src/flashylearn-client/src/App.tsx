2c3bfae5ecbbe9932c4839663d5b4a09ca808aa0
Ever Orellana <ever1509@gmail.com>
2022-12-15 22:42:42 -0600

    init project with material ui

--- src/flashylearn-client/src/App.tsx
+++ src/flashylearn-client/src/App.tsx
@@ -1,14 +1,8 @@
 import React from 'react';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          It's working
-        </p>
-      </header>
+    <div>      
     </div>
   );
 }

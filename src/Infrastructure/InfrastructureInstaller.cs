539236856988eb80fc9ecd125f644f9140508d09
Ever Orellana <ever1509@gmail.com>
2023-06-07 23:10:37 -0600

    :sparkles: updating docker compose to use external port

--- src/Infrastructure/InfrastructureInstaller.cs
+++ src/Infrastructure/InfrastructureInstaller.cs
@@ -1,11 +1,9 @@
-using System.Data;
 using Application.Common.Interfaces;
 using Infrastructure.Data;
 using Infrastructure.Data.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Npgsql;
 
 namespace Infrastructure;
 
@@ -13,15 +11,15 @@ public static class InfrastructureInstaller
 {
     public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
-        
+        var connectionString = Environment.GetEnvironmentVariable("DB_CONNECTION_STRING");
         services.AddDbContextFactory<FlashyLearnContext>(options =>
             options.UseNpgsql(
-                configuration.GetConnectionString("FlashyConnPostgresSQL"),
+                connectionString,
                 b => b.MigrationsAssembly(typeof(FlashyLearnContext).Assembly.FullName)));
 
         services.AddDbContext<FlashyLearnContext>(options =>
             options.UseNpgsql(
-                configuration.GetConnectionString("FlashyConnPostgresSQL"),
+                connectionString,
                 b => b.MigrationsAssembly(typeof(FlashyLearnContext).Assembly.FullName)));
         services.AddScoped<IUnitOfWork>(
             sp => sp.GetRequiredService<IDbContextFactory<FlashyLearnContext>>()

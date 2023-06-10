9e42307510ca9dbf7fbe15b52540e46f6a628bd6
Ever Orellana <ever1509@gmail.com>
2023-06-09 19:15:26 -0600

    :sparkles: adding select reusable component

---
+++ src/flashylearn-client/src/components/FormsUI/OmSelect.tsx
@@ -0,0 +1,43 @@
+import { MenuItem, TextField } from "@mui/material";
+import { useField, useFormikContext } from "formik";
+
+interface OmSelectProps{
+    name: string,
+    options: any,
+    otherProps: any
+}
+
+export default function OmSelect({name, options, otherProps}: OmSelectProps){
+    const {setFieldValue} = useFormikContext();
+    const [field, meta] = useField(name);
+
+    function handleChange(event: any){
+        const {value} = event.target;
+        setFieldValue(name, value);
+    }
+
+    const configSelect = {
+        ...field, 
+        ...otherProps,
+        select: true,
+        variant: 'outlined',
+        onChange: handleChange
+    }
+
+    if(meta && meta.touched && meta.error){
+        configSelect.error = true;
+        configSelect.helperText = meta.error;
+    }
+
+    return(
+        <TextField {...configSelect}>
+            {Object.keys(options).map((item, pos)=>{
+                return (
+                    <MenuItem key={pos} value={item}>
+                        {options[item]}
+                    </MenuItem>
+                )
+            })}
+        </TextField>
+    )
+}

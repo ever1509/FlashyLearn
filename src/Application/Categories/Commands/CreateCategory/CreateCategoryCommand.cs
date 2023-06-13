b5b8286c8eeacb2bf75fb2b3ee37e2e68dbd29fa
Ever Orellana <ever1509@gmail.com>
2023-06-12 23:04:19 -0600

    :sparkles: adding some updates in create category form

--- src/Application/Categories/Commands/CreateCategory/CreateCategoryCommand.cs
+++ src/Application/Categories/Commands/CreateCategory/CreateCategoryCommand.cs
@@ -7,6 +7,7 @@ namespace Application.Categories.Commands.CreateCategory;
 
 public class CreateCategoryCommand : IRequest<CategoryResponseDto>
 {
+    public string CategoryID { get; set; } = string.Empty;
     public string Name { get; set; } = string.Empty;
     public string? UserId { get; set; } = string.Empty;
 }
--- src/flashylearn-client/src/features/categories/categoryForms/CategoryForm.tsx
+++ src/flashylearn-client/src/features/categories/categoryForms/CategoryForm.tsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
-import { CategoryDto } from "../../../graphql/generated/schema";
+import { CategoryDto, CategoryResponseDto, CreateCategoryCommandInput, useCreateCategoryMutation } from "../../../graphql/generated/schema";
 import * as yup from 'yup';
 import { useNavigate } from "react-router-dom";
-import { Container, Grid } from "@mui/material";
+import { Alert, Container, Grid, Snackbar } from "@mui/material";
 import { Form, Formik } from "formik";
 import OmTextField from "../../../components/FormsUI/OmTextField";
 import OmSubmitButton from "../../../components/FormsUI/OmSubmitButton";
+import OmLoading from "../../../components/elements/OmLoading";
+import OmAlert from "../../../components/elements/OmAlert";
 
 interface CategoryFormProps{
     category: CategoryDto
@@ -17,11 +19,6 @@ const FORM_VALIDATION = yup.object().shape({
 });
 
 export default function CategoryForm({category}: CategoryFormProps){
-    function createCategory(values: any){
-        console.log(values);
-    }
-
-
     const [open, setOpen] = useState(false);
     const navigate = useNavigate();
 
@@ -30,11 +27,50 @@ export default function CategoryForm({category}: CategoryFormProps){
         name: category.name || ''
     }
 
+    const [createCategory, {loading: createCategoryLoading, error: createCategoryError}] = useCreateCategoryMutation();
+
+    const handleClose = (event: any) => {
+        if(event.reason === 'clickaway'){
+            return;
+        }
+        setOpen(false);
+    }
+
+    async function createCategoryDetails(values: CreateCategoryCommandInput){
+        const response = await (createCategory({variables: {
+            category: values
+        }}));
+
+        setOpen(true);
+
+        const category = response.data?.createCategory as CategoryResponseDto;
+
+        if(category.categoryId){
+            navigate(`/categories/${category.categoryId}`);
+        }
+
+        if(createCategoryLoading){
+            return (<OmLoading />)
+        }
+
+        if(createCategoryError){
+            <Snackbar open={true} autoHideDuration={6000}>
+                <Alert security="error">Error Retrieving category data</Alert>
+            </Snackbar>
+        }
+        
+    }
+
     return (<Container>
+        <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+            <Alert onClose={handleClose} severity="success" sx={{width : '100%'}}>
+                {!category.categoryID ? "Category successfully added!": "Category successfully updated!"}
+            </Alert>
+        </Snackbar>
         <div>
             <Formik initialValues={INITIAL_FORM_STATE}
             validationSchema={FORM_VALIDATION}
-            onSubmit={createCategory}>
+            onSubmit={createCategoryDetails}>
                 <Form>
                     <Grid container spacing={2} >
                         <Grid item xs={6}>
